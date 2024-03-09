a86a79018f5da9652d4f74b2c2677a8573aa74b8
lrm <756131502@qq.com>
2024-03-09 23:14:47 +0800

    python - 变量和关键字

---
+++ part1/keyword.py
@@ -0,0 +1,9 @@
+# coding:utf-8
+# python 常见的关键字 在README.md中有记录
+
+uname = "lucky"
+
+# print = 1
+
+if __name__ == '__main__':
+    print(uname)
---
+++ part1/varable.py
@@ -0,0 +1,16 @@
+# coding:utf-8
+
+username = '小明'
+age = 20
+
+is_sleep = 'ok'
+
+dog_exists = 'y'
+
+a, b, c = 1, 2, 3
+
+if __name__ == '__main__':
+    print(username)
+    print(age)
+    print(is_sleep)
+    print(dog_exists)
