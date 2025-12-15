6ecfacfbf7df3f5f65794a2ffc4cf068b25cd4eb
ming <756131502@qq.com>
2025-12-15 23:09:14 +0800

    异常错误状态统一处理

--- llmops/llmops-api/internal/exception/__init__.py
+++ llmops/llmops-api/internal/exception/__init__.py
@@ -5,3 +5,21 @@
 @Author : 756131502@qq.com
 @File   : __init__.py.py
 """
+
+from .exception import (
+    CustomException,
+    FailException,
+    NotFoundException,
+    UnauthorizedException,
+    ForbiddenException,
+    ValidateErrorException
+)
+
+__all__ = [
+    "CustomException",
+    "FailException",
+    "NotFoundException",
+    "UnauthorizedException",
+    "ForbiddenException",
+    "ValidateErrorException"
+]
--- llmops/llmops-api/internal/handler/app_handler.py
+++ llmops/llmops-api/internal/handler/app_handler.py
@@ -10,6 +10,7 @@ import os
 from flask import request
 from openai import OpenAI
 
+from internal.exception import FailException
 from internal.schema.app_schema import CompletionReq
 from pkg.response import success_json, validate_error_json
 
@@ -47,4 +48,5 @@ class AppHandler:
         return success_json({"content": content})
 
     def ping(self):
-        return {"ping": "pink"}
+        raise FailException("数据没有找到")
+        # return {"ping": "pink"}
