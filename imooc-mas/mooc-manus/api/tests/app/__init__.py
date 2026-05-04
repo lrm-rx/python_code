365770038789122d745ee652dbcf0a8355c5a430
ming <756131502@qq.com>
2026-05-04 16:53:31 +0800

    添加测试

--- imooc-mas/mooc-manus/api/tests/app/__init__.py
+++ imooc-mas/mooc-manus/api/tests/app/__init__.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-@Time   : 2026/05/03 0003 18:46
+@Time   : 2026/05/04 0004 16:52
 @Author : 756131502@qq.com
 @File   : __init__.py.py
 """
-from .base import Base
-
-__all__ = [
-    "Base"
-]
