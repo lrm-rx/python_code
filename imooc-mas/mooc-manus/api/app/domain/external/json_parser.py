fc127e5e3800ef879d016b573bcdade8a8e6dcbd
ming <756131502@qq.com>
2026-05-07 22:33:40 +0800

    LLM结构化输出缺陷与JSON修复解析器开发

---
+++ imooc-mas/mooc-manus/api/app/domain/external/json_parser.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Time   : 2026/05/07 0007 22:29
+@Author : 756131502@qq.com
+@File   : json_parser.py
+"""
+from typing import Protocol, Optional, Any, Union, Dict, List
+
+
+class JSONParser(Protocol):
+    """JSON解析器，用于解析json字符串并修复"""
+
+    async def invoke(self, text: str, default_value: Optional[Any] = None) -> Union[Dict, List, Any]:
+        """调用函数，用于将传递进来的文本进行解析并返回"""
+        ...
---
+++ imooc-mas/mooc-manus/api/app/infrastructure/external/json_parser/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Time   : 2026/05/07 0007 22:30
+@Author : 756131502@qq.com
+@File   : __init__.py.py
+"""
